6b8acca9d032a69c27fcd3a790ec0ee7b0d290ee
juanignaciogarciabartolome <juanigb3@hotmail.com>
2023-12-31 16:18:46 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import LandingPage from './Pages/Landing/LandingPage';
 import StudyGuide from './Pages/Guide/StudyGuide';
 import ExamWrapper from './Pages/Exam/ExamWrapper';
 
 const App: React.FC = () => {
     return (
-        <Router basename="/plazop">
+        <HashRouter basename="/plazop">
             <Routes>
                 <Route path="/" element={<LandingPage/>} />
                 <Route path="/guia" element={<StudyGuide/>} />
                 <Route path="/examen" element={<ExamWrapper/>} />
             </Routes>
-        </Router>
+        </HashRouter>
     );
 };
 
